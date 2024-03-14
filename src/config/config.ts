a600f9a19fc53fc26bfd26bc058e8e2862fa889e
Billal2141884 <161714794+Billal2141884@users.noreply.github.com>
2024-03-14 13:19:40 +0000

    configured with blockchain

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,17 @@
-// your config code
+import algosdk from "algosdk";
+
+const algoToken = "a".repeat(64);
+const server: string = "http:/localhost";
+const port: string = "4001";
+
+const mnemonic : string = "green frame drama alpha type regular credit goose gravity alone luxury end reject skirt west outside truly funny conduct obtain jacket ostrich artefact ability spy";
+
+export function getClient(): algosdk.Algodv2 {
+    let client = new algosdk.Algodv2(algoToken, server, port);
+    return client;
+};
+
+export function getAccount(): algosdk.Account {
+    let account = algosdk.mnemonicToSecretKey(mnemonic);
+    return account;
+};
